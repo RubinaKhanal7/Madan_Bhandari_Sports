b235fbd0cad2a78d61541c5aeaf52ac366a61b67
siyanaa <adhikari.siyana@gmail.com>
2025-01-06 17:01:19 +0545

    User/Verification/Approval.

--- database/migrations/2024_02_01_075212_create_users_table.php
+++ database/migrations/2024_02_01_075212_create_users_table.php
@@ -16,11 +16,13 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email')->unique()->nullable();
+            $table->string('phonenumber')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
+            $table->string('pin')->nullable();
             $table->integer('is_active')->default(1);
-            $table->unsignedBigInteger('role')->default(3);
+            $table->unsignedBigInteger('role')->default(1);
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
