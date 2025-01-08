9258f39bacf5ed647db10aeba67e9c525229c9f8
siyanaa <adhikari.siyana@gmail.com>
2025-01-08 15:52:47 +0545

    Video Gallery.

--- app/Models/VideoGallery.php
+++ app/Models/VideoGallery.php
@@ -2,24 +2,32 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
 class VideoGallery extends Model
 {
     use HasFactory;
 
-    // Specify the fillable fields
     protected $fillable = [
-        'title_ne',
-        'title_en',
-        'videos',
-        'url',
-        'description_ne',
-        'description_en',
-        'is_featured',
-        'is_active',
+        'title_ne', 
+        'title_en', 
+        'videos', 
+        'url', 
+        'description_ne', 
+        'description_en', 
+        'is_featured', 
+        'is_active', 
+        'meta_data_id',
     ];
 
-   
+    protected $casts = [
+        'is_featured' => 'boolean',
+        'is_active' => 'boolean'
+    ];
+
+    public function metaData()
+    {
+        return $this->belongsTo(MetaData::class, 'meta_data_id');
+    }
 }
